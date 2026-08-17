5ae67a79df05aaca1cb726bbfd11776115eb43ea
Ivan Mikhailov <smplgdev@gmail.com>
2026-08-17 23:40:24 +0200

    feat: add get http method

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,6 +3,41 @@ const API_BASE_URL = process.env.REACT_APP_API_URL || ''  // TODO: maybe move to
 const TOKEN_KEY = 'app-token'
 const USER_KEY = 'app-userId'
 
+
+async function get(path, jwt=null) {
+    let response = null
+    let headers = {
+        'Content-Type': 'application/json'
+    }
+    if (jwt !== null) {
+        headers = {
+            ...headers,
+            'Authorization': `Bearer ${jwt}`
+        }
+    }
+    try {
+        response = await fetch(
+            `${API_BASE_URL}${path}`,
+            {
+                method: "GET",
+                headers,
+            }
+        )
+    } catch (err) {
+        throw new Error('Could not reach the server')
+    }
+
+    let data = {}
+    try {
+        data = await response.json()
+    } catch(_) {}
+
+    if (!response.ok) {
+        throw new Error(data.error || `Request failed (${response.status})`)
+    }
+    return data
+}
+
 async function post(path, body) {
     let response = null
     try {
@@ -28,6 +63,7 @@ async function post(path, body) {
 
 export {
     post,
+    get,
     TOKEN_KEY,
     USER_KEY
 }
