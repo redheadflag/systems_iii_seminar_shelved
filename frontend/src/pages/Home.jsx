0306ed671909e5aa378522556422c8ca0c3d4b26
Ivan Mikhailov <smplgdev@gmail.com>
2026-08-20 20:39:59 +0200

    feat: add text to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,22 @@ export default function Home() {
     return (
         <>
             <AppShell>
-                <h1>Hello world</h1>
+                <div className="page page--reading">
+                    <div className="d-flex flex-column gap-3">
+                        <p className="lead fs-2 mb-0">
+                            Have you been building a collection for a long time but have no one to share it with?{" "}
+                            <span className="accent">Shelved</span> is the perfect online showcase for your collection.
+                        </p>
+
+                        <p className="muted fs-4 mb-0">
+                            Shelved lets you create <strong className="accent">public and private collections</strong>, comment on other users' collections, and share your thoughts on what you see.
+                        </p>
+
+                        <p className="muted fs-4 mb-0">
+                            And for the most discerning collectors, a feature allowing exchanges with other users is coming soon.
+                        </p>
+                    </div>
+                </div>
             </AppShell>
         </>  
     ) 
